4387ff6d50bb0251de0ee800c0d651e61ccc0071
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-06-14 21:30:02 +0100

    Add review request feature

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -11,3 +11,37 @@ $('.vrBtn').click(function(event){
 $('#close').click(function(event){
     $('#ridedetails').css('left', '100%');
 });
+
+$('.accept').click(function(event){
+    var initT = new Date().getSeconds();
+
+    var timer = setInterval(function(){
+        var t = new Date().getSeconds();
+        if(t - initT === 2){
+            $('#message').html('<p>You have successfully added <b>Rider Name</b> to the Trip.</p>');
+            $('#message').css('visibility', 'visible');
+        }
+
+        if(t - initT === 5){
+            $('#message').css('visibility', 'hidden');
+            clearInterval(timer);
+        }
+    }, 1000);
+});
+
+$('.reject').click(function(event){
+    var initT = new Date().getSeconds();
+
+    var timer = setInterval(function(){
+        var t = new Date().getSeconds();
+        if(t - initT === 2){
+            $('#message').html('<p>You rejected <b>Rider Name</b>\'s request.</p>');
+            $('#message').css('visibility', 'visible');
+        }
+
+        if(t - initT === 5){
+            $('#message').css('visibility', 'hidden');
+            clearInterval(timer);
+        }
+    }, 1000);
+});
