091fded6528b861c6984e9a1cde400518dc80157
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-06-14 12:41:18 +0100

    Add view ride offer feature

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -3,3 +3,11 @@ $('#sign-in-btn').click(function(event){
     console.log("Me");
     window.location.replace('dashboard.html');
 });
+
+$('.vrBtn').click(function(event){
+    $('#ridedetails').css('left', '80%');
+});
+
+$('#close').click(function(event){
+    $('#ridedetails').css('left', '100%');
+});
