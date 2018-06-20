57860e64636790376dd051597eb7e43d5e3ddc47
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-06-20 13:23:10 +0100

    Remove repetitions

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,22 +2,20 @@ const Request = require('request');
 require('../server/app');
 
 describe('Server', () => {
-  describe('POST-Requests', () => {
-    const data = {};
-    beforeAll((done) => {
-      Request.get('http://localhost:3000/api/v1/rides', (error, res, body) => {
-        data.status = res.statusCode;
-        data.rides = JSON.parse(body);
-        done();
-      });
+  const data = {};
+  beforeAll((done) => {
+    Request.get('http://localhost:3000/api/v1/rides', (error, res, body) => {
+      data.status = res.statusCode;
+      data.rides = JSON.parse(body);
+      done();
     });
+  });
 
-    it('Status 200', () => {
-      expect(data.status).toBe(200);
-    });
+  it('Status 200', () => {
+    expect(data.status).toBe(200);
+  });
 
-    it('Number of rides', () => {
-      expect(data.rides.length).toEqual(4);
-    });
+  it('Number of rides', () => {
+    expect(data.rides.length).toEqual(4);
   });
 });
