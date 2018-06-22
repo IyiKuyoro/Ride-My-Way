318c980001a98293c2245044c8ec319198443185
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-06-22 03:17:12 +0100

    Remove get request for specific ride

--- server/app.js
+++ server/app.js
@@ -24,18 +24,4 @@ app.get('/api/v1/rides', (req, res) => {
   res.json(rides);
 });
 
-app.get('/api/v1/rides/:rideId', (req, res) => {
-  let success = false;
-  rides.forEach((element) => {
-    if (element.rideID === req.params.rideId) {
-      success = true;
-      res.json(element);
-    }
-  });
-
-  if (!success) {
-    res.status(404).send('Information not found. Perhaps try to get the avaliable rides first and select an ID.');
-  }
-});
-
 app.listen(3000);
