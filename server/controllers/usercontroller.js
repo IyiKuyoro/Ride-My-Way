10e7552b6d08511ae26359ae03153b3054f456b2
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-06-28 10:46:30 +0100

    Refactore controler to query db directly

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,4 +1,7 @@
-import users from '../model/dbUserModels';
+// import users from '../model/dbUserModels';
+import client from '../model/db';
+
+const bcrypt = require('bcrypt');
 
 const validEmail = (email) => {
   if (email) {
@@ -7,25 +10,40 @@ const validEmail = (email) => {
   return false;
 };
 
+const genID = () => `U_${Math.floor(Math.random() * 9000000000) + 1000000000}`;
+
 const controller = {
   postSuccess: false,
   postSignUp: (req, res) => {
     if (validEmail(req.body.EmailAddress)) {
-      users.addUser(req.body);
-      console.log(controller.postSuccess);
-    }
-
-    if (controller.postSuccess) {
-      const response = {
-        token: '',
-        status: 'Success',
-        body: 'user added to database',
-      };
-      res.json(response);
-    } else {
-      res.json({
-        status: 'Fail',
-        message: 'Could not add user to database',
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          console.log(err);
+        }
+        bcrypt.hash(req.body.Password, salt, (error, hash) => {
+          if (err) {
+            console.log(error);
+          }
+          const text = 'INSERT INTO public."Users" ("ID", "FirstName", "LastName", "Sex", "DOB", "MobileNumber", "EmailAddress", "Password", "RidesOffered", "RidesTaken", "Friends") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *;';
+          const values = [genID(), req.body.FirstName, req.body.LastName, req.body.Sex, req.body.DOB, req.body.PhoneNumber, req.body.EmailAddress, hash, 0, 0, 0];
+          client.query(text, values, (error1, result) => {
+            if (error1) {
+              res.json({
+                status: 'Fail',
+                message: 'Could not add user to database',
+              });
+              console.log(error1.stack);
+            } else {
+              const response = {
+                token: '',
+                status: 'Success',
+                body: 'user added to database',
+              };
+              res.json(response);
+              console.log(result.rows[0]);
+            }
+          });
+        });
       });
     }
   },
