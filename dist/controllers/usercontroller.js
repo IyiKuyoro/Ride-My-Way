9b81f6f05d23d4efbc9b04b8ad0508b0ffeea5c8
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-01 08:37:32 +0100

    Build

--- dist/controllers/usercontroller.js
+++ dist/controllers/usercontroller.js
@@ -12,6 +12,10 @@ var _jsonwebtoken = require('jsonwebtoken');
 
 var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);
 
+var _bcrypt = require('bcrypt');
+
+var _bcrypt2 = _interopRequireDefault(_bcrypt);
+
 var _helper = require('../helpers/helper');
 
 var _helper2 = _interopRequireDefault(_helper);
@@ -22,7 +26,6 @@ var _db2 = _interopRequireDefault(_db);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// import bcrypt from 'bcrypt';
 _dotenv2.default.config();
 
 var genID = function genID() {
@@ -32,36 +35,46 @@ var controller = {
   postSignUp: function postSignUp(req, res) {
     _helper2.default.validEmail(req.body.EmailAddress, function (valid) {
       if (valid) {
-        var text = 'INSERT INTO public."Users" ("ID", "FirstName", "LastName", "Sex", "DOB", "MobileNumber", "EmailAddress", "Password", "RidesOffered", "RidesTaken", "Friends") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *;';
-        var values = [genID(), req.body.FirstName, req.body.LastName, req.body.Sex, req.body.DOB, req.body.PhoneNumber, req.body.EmailAddress, req.body.Password, 0, 0, 0];
-        _db2.default.query(text, values, function (error1, result) {
-          if (error1) {
-            res.status(400);
-            res.json({
-              status: 'fail',
-              message: 'Could not add user to database'
-            });
-          } else {
-            var token = _jsonwebtoken2.default.sign({
-              email: result.rows[0].EmailAddress,
-              userId: result.rows[0].ID
-            }, process.env.KEY, {
-              expiresIn: '1h'
-            });
-            var response = {
-              token: token,
-              status: 'Success',
-              data: {
-                ID: result.rows[0].ID,
-                FirstName: result.rows[0].FirstName,
-                LastName: result.rows[0].LastName,
-                Sex: result.rows[0].Sex,
-                DOB: result.rows[0].DOB,
-                EmailAddress: result.rows[0].EmailAddress
-              }
-            };
-            res.json(response);
+        _bcrypt2.default.genSalt(10, function (err, salt) {
+          if (err) {
+            console.log(err);
           }
+          _bcrypt2.default.hash(req.body.Password, salt, function (error, hash) {
+            if (err) {
+              console.log(error);
+            }
+            var text = 'INSERT INTO public."Users" ("ID", "FirstName", "LastName", "Sex", "DOB", "MobileNumber", "EmailAddress", "Password", "RidesOffered", "RidesTaken", "Friends") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *;';
+            var values = [genID(), req.body.FirstName, req.body.LastName, req.body.Sex, req.body.DOB, req.body.PhoneNumber, req.body.EmailAddress, hash, 0, 0, 0];
+            _db2.default.query(text, values, function (error1, result) {
+              if (error1) {
+                res.status(400);
+                res.json({
+                  status: 'fail',
+                  message: 'Could not add user to database'
+                });
+              } else {
+                var token = _jsonwebtoken2.default.sign({
+                  email: result.rows[0].EmailAddress,
+                  userId: result.rows[0].ID
+                }, process.env.KEY, {
+                  expiresIn: '1h'
+                });
+                var response = {
+                  token: token,
+                  status: 'Success',
+                  data: {
+                    ID: result.rows[0].ID,
+                    FirstName: result.rows[0].FirstName,
+                    LastName: result.rows[0].LastName,
+                    Sex: result.rows[0].Sex,
+                    DOB: result.rows[0].DOB,
+                    EmailAddress: result.rows[0].EmailAddress
+                  }
+                };
+                res.json(response);
+              }
+            });
+          });
         });
       } else {
         res.status(400);
