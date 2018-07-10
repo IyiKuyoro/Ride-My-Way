661a32f741480e049d37653b6e281a707693c109
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-10 18:48:53 +0100

    Remove useless files

--- server/controllers/controller.js
+++
@@ -1,7 +0,0 @@
-const controller = {
-  get: (req, res) => {
-    res.send('Server is running, kindly use the endpoints. /api/v1/rides, /api/v1/rides/:rideId, /api/v1/rides, /api/v1/rides/:rideId/requests');
-  },
-};
-
-export default controller;
