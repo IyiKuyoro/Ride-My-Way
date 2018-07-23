1fa4e52816e831fa3a140b6bd0b9ef28c4be5be6
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-23 15:33:21 +0100

    Attempt to correct jwt middleware problem

--- server/app.js
+++ server/app.js
@@ -8,14 +8,6 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(apiRoutes);
-app.use((req, res, done) => {
-  res.header('Accept', 'application/json, text/plain, */*');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, jwt');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET');
-  res.header('Content-Type', 'application/json');
-  done();
-});
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
